4e7e3573c0c343e06c1c21d7f5d890648a5f7de8
emma-rodriguez-z <emmarodriguez@bren.ucsb.edu>
2026-05-26 13:31:46 -0700

    Emmas Nutrient Model

--- scripts/nutrient_module_script.r
+++ scripts/nutrient_module_script.r
@@ -1,3 +1,41 @@
 # Script for Nutrient Module
 
+# Classifies lake nutrient status based on total phosphorus and total nitrogen
+#
+# ---- Inputs:---- 
+#   total_p: total phosphorus in mg/L
+#   total_n: total nitrogen in mg/L
+#
+#----- Output: ----
+#   A nutrient classification:
+#   "Oligotrophic", "Mesotrophic", "Eutrophic", or "Hypereutrophic"
+
+
+nutrient_model <- function(total_p, total_n) {
+  
+  if (total_p < 0.01 && total_n < 0.35) {
+    nutrient_class <- "Oligotrophic"
+    
+  } else if (total_p >= 0.01 && total_p <= 0.03 &&
+             total_n >= 0.36 && total_n <= 0.65) {
+    nutrient_class <- "Mesotrophic"
+    
+  } else if (total_p > 0.03 && total_p <= 0.10 &&
+             total_n > 0.65 && total_n <= 1.20) {
+    nutrient_class <- "Eutrophic"
+    
+  } else if (total_p > 0.10 || total_n > 1.20) {
+    nutrient_class <- "Hypereutrophic"
+    
+  } else {
+    nutrient_class <- "Mixed or unclear nutrient condition"
+  }
+  
+  return(nutrient_class)
+}
+
+
+
+
+
 
