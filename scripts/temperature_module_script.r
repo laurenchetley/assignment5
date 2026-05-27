e8b01d61edf0305d732398b9c8b437fd776e3fa9
laurenchetley <laurenchetley@gmail.com>
2026-05-26 22:12:43 -0700

    Commiting this

--- scripts/temperature_module_script.r
+++ scripts/temperature_module_script.r
@@ -8,16 +8,14 @@ library(here)
 library(ggplot2)
 library(janitor)
 
-
-#classification water temperature 
+classification water temperature 
 temp_classification<- function(mean_temp_c){
   if(mean_temp_c<13){temp_class<-"Cool - Below Average"}
   else if(mean_temp_c>=13&&mean_temp_c<=22){temp_class<-"Mild - Average"}
   else if (mean_temp_c>22&&mean_temp_c<=30){temp_class<-"Warm - Above Average"}
   else if (mean_temp_c>30){temp_class<-"Hot - Boiling"}
   return(temp_class)
-  }
-
+}
 
 
 
