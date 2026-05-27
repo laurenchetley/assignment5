65ea4f708a88b570bcb2b0b748ea67ec9a42a41e
laurenchetley <laurenchetley@gmail.com>
2026-05-26 22:04:35 -0700

    classification commit for surface water temp

--- scripts/temperature_module_script.r
+++ scripts/temperature_module_script.r
@@ -1,2 +1,33 @@
 # Script for Temperature Module
 
+
+#loading in the libraries 
+library(tidyverse)
+library(dplyr)
+library(here)
+library(ggplot2)
+library(janitor)
+
+calculate_water_temp <-function(dates,temperatures){
+  if(length(dates)!=length(temperatures)){
+    stop("Dates and Temperature must be the same length.")
+  }
+ df
+}
+
+
+
+
+
+##fake data 
+## Generate fake dates (one full water year: Oct 1 - Sep 30)
+#dates <- seq.Date(as.Date("2023-10-01"), as.Date("2024-09-30"), by = "day")
+
+## Generate fake temperatures (seasonal sine wave + some random noise)
+#set.seed(42)
+#temperatures <- 15 + 10 * sin(seq(0, 2 * pi, length.out = 365)) + rnorm(365, 0, 2)
+
+## Run the function
+#result <- calculate_water_temp_summary(dates = dates, temperatures = temperatures)
+
+#print(result)
