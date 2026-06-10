468393272ba5105b552955058bd9284123fc7d85
emma-rodriguez-z <emmarodriguez@bren.ucsb.edu>
2026-06-09 21:24:36 -0700

    tests

--- tests/running_tests.R
+++ tests/running_tests.R
@@ -3,11 +3,11 @@ library(here)
 
 test_dir(here("tests"))
 
-test_file(here("tests/test_temp_model.R"))
+test_file(here("tests/test_temp_function.R"))
 
-test_file(here("tests/test_oxygen_modeln.R"))
+test_file(here("tests/test_oxygen_function.R"))
 
-test_file(here("tests/test_nutrient_model.R"))
+test_file(here("tests/test_nutrient_function.R"))
 
 test_dir(here("tests"))
 
